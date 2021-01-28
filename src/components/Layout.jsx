0d5becb80335623eeea1acfcee2bf3adab0d8602
Mike <magrozero@yandex.ru>
2021-01-28 21:58:33 +0300

    add img on background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,9 @@
 import s from "./Layout.module.css"
 
 const Layout = ({id, title, descr, urlBg, colorBg}) => {
-  const style = {backgroundColor: colorBg}
+  const style = {}
+  if (colorBg) style.backgroundColor = colorBg
+  if (urlBg) style.backgroundImage = `url(${urlBg}`
   return (
     <section className={s.root} id={id} style={style}>
       <div className={s.wrapper}>
